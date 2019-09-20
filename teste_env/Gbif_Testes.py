b95e2fca082bb459fd9637786133d49449ae3c4e
Elielson68 <elielsonbr.com@gmail.com>
2019-09-20 16:53:21 -0300

    Teste commit de casa

--- teste_env/Gbif_Testes.py
+++ teste_env/Gbif_Testes.py
@@ -1,3 +1,38 @@
-import pygbif as occ
+import pygbif
+occ = pygbif
+pesquisa = occ.search(geometry='POLYGON((-60.2910 -14.4626,-52.6142 -14.4626, -53.5810 -22.2995,  -60.1591 -22.2995, -60.2910 -14.4626))', scientificName="Anodorhynchus hyacinthinus")
+print(pesquisa)
+for x in pesquisa['results']:
+    Resultado = ""
+    if 'countryCode' in x:
+        Resultado += "Nome científico: "+x['scientificName']
+    else:
+        Resultado += "Nome científico: NULO"
 
-occ.search(scientificName="Anodorhynchus hyacinthinus",institutionCode="")
+    if 'countryCode' in x:
+        Resultado += " | Código do País: "+x['countryCode']
+    else:
+        Resultado += " | Código do País: NULO"
+
+    if 'country' in x:
+        Resultado += " | País: "+x['country']
+    else:
+        Resultado += " | País: NULO"
+
+    if 'stateProvince' in x:
+        Resultado += " | Estado: "+x['stateProvince']
+    else:
+        Resultado += " | Estado: NULO"
+
+    if 'decimalLatitude' in x:
+        Resultado += " | Latitude: "+str(x['decimalLatitude'])
+    else:
+        Resultado += " | Latitude: NULO"
+
+    if 'decimalLongitude' in x:
+        Resultado += " | Longitude: "+str(x['decimalLongitude'])
+    else:
+        Resultado += " | Longitude: NULO"
+
+    print(Resultado+"\n")
+    print("Teste de commit, bebê")
