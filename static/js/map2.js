da02d6d3a7315229d3a622ccb69f995f113891f1
Elielson68 <elielsonbr.com@gmail.com>
2019-10-18 16:07:12 -0300

    separação dos arquivos

---
+++ static/js/map2.js
@@ -0,0 +1,5 @@
+function MapaDois() {
+    var belem = {lat:-1.44502, lng: -48.4650};
+    var map = new google.maps.Map(document.getElementById('mapa_plotar'), {zoom: 4, center: belem});
+    alert(poligono_criado)
+}
