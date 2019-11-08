7bddc1ec3825a2fe1447a302928f2b4d8427c4fa
Elielson <elielsonbr.com@gmail.com>
2019-11-07 23:56:44 -0300

    tentar aplicar o nivel de hierarquia

--- planilha.py
+++ planilha.py
@@ -196,7 +196,8 @@ class Tratamento_de_Dados:
             else:
                 Scientific_Name = NC_value[0][nome]+" "+NC_value[1][nome]
                 print(Scientific_Name)
-                valores = requests.get('http://api.gbif.org/v1/species/match?kingdom=&phylum=&order=&family=&genus=&name=Anodorhynchus%20hyacinthinus'+Scientific_Name).json()
+                #'http://api.gbif.org/v1/species/match?kingdom=''&phylum=''&order=''&family=''&genus=''&name=''Anodorhynchus hyacinthinus
+                valores = requests.get('http://api.gbif.org/v1/species/match?name='+Scientific_Name).json()
                 if(valores["matchType"] != "NONE"):
                     if(valores["matchType"] == "FUZZY"):
                         self.ocorrencias_NC[Scientific_Name] = {"quantidade": NC_value[0].count(NC_value[0][nome]), "precisão": valores["confidence"], "corretude": valores["matchType"], "sugerirCorrecao": valores["canonicalName"]}
