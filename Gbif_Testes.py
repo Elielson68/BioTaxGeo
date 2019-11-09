9d61920d9efd1bf7923d614f7397b001b49049c3
Elielson <elielsonbr.com@gmail.com>
2019-11-08 23:53:38 -0300

    nada em especial

--- Gbif_Testes.py
+++ Gbif_Testes.py
@@ -18,9 +18,9 @@ def mapa_teste():
         f = request.files['file']
         f.save(secure_filename(f.filename))
         planilha_atual.set_Diretorio(secure_filename(f.filename))
-        #planilha_atual.set_ColG_ColNC("Genus1","Species1")
-        
-        return planilha_atual.tratamento_de_dados.Verificar_similaridade_de_string(4)
+        planilha_atual.set_ColG_ColNC("Genus1","Species1")
+        #planilha_atual.tratamento_de_dados.Verificar_similaridade_de_string(4)
+        return planilha_atual.get_NC_Tratado()
 
 #'POLYGON(([longitude ->]-60.2910 [latitude ->]-14.4626,-52.6142 -14.4626, -53.5810 -22.2995,  -60.1591 -22.2995, -60.2910 -14.4626))'
 
