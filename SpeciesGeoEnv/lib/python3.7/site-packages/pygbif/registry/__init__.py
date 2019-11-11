f0a7e60ef9f8916e2dbb04837c47559b4e87f568
Elielson68 <elielsonbr.com@gmail.com>
2019-11-11 14:35:16 -0300

    SpecieGeoEnv virtualenv add in project of git

---
+++ SpeciesGeoEnv/lib/python3.7/site-packages/pygbif/registry/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .nodes import nodes
+from .networks import networks
+from .installations import installations
+from .datasets import datasets, dataset_metrics, dataset_suggest, dataset_search
+from .organizations import organizations
