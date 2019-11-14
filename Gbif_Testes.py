3446b7fb2a75f2a949f68d7d7b6e55491459f4fa
Elielson68 <elielsonbr.com@gmail.com>
2019-11-14 14:50:29 -0300

    criando formulario

--- Gbif_Testes.py
+++ Gbif_Testes.py
@@ -1,9 +1,12 @@
 #'''
 import pygbif
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, send_file, jsonify, redirect, url_for, current_app, send_from_directory
 from werkzeug.utils import secure_filename
 from planilha import Planilha
 import json
+import  os
+import tkinter.filedialog
+
 occ = pygbif
 app = Flask(__name__)
 planilha_atual = Planilha()
@@ -26,18 +29,16 @@ def mapa_teste():
         print(type(verificacao))
         
         return render_template("planilha.html", titulos=titulos, verificacao=verificacao)
-'''
-@app.route("/salvar", methods=["POST"])
-def mapa_teste():
-    if request.method == "POST":
-        dados = request.files['dados']
-        dados = eval(dados)
-        for indice in range(0, len(dados)):
-            planilha_atual.tratamento_de_dados.AlterandoDadosPlanilha()
 
-        #planilha_atual.tratamento_de_dados.SalvarPlanilhaFormatada()
-        return "eae"
-'''
+
+
+@app.route('/salvar', methods=['GET', 'POST'])
+def download():
+    teste = tkinter.filedialog.
+    return "hm"
+
+
+
 #'POLYGON(([longitude ->]-60.2910 [latitude ->]-14.4626,-52.6142 -14.4626, -53.5810 -22.2995,  -60.1591 -22.2995, -60.2910 -14.4626))'
 
 app.run(debug=True, port=8080)
--- teste.py
+++
@@ -1,3 +0,0 @@
-texto = "Elielson"
-teste2 = "Elielson Fernando"
-print(teste2 in texto)
