104bd2b31e46ae7240f3d31b8f06e7e94301ceea
Renan <renanfigcarneiro@gmail.com>
2020-02-18 11:27:09 -0300

    style: index.html

---
+++ static/js/loading.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader");
+  setTimeout(() => {
+      
+  loader.className += " hidden";
+  }, 1500);
+      }
+  );
