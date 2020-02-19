ced60dc0319ca8667620827a551e9f30ffa85f42
Renan <renanfigcarneiro@gmail.com>
2020-02-19 12:07:38 -0300

    style: Selecionar_Rota

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,8 +1,7 @@
 window.addEventListener("load", function () {
+
   const loader = document.querySelector(".loader");
-  setTimeout(() => {
-      
-  loader.className += " hidden";
-  }, 1500);
-      }
+  setTimeout(() => {loader.className += " hidden";}, 1500);
+  
+ }
   );
