fe8b7a5189a66a331fe18fcd0159f9d9e879adec
Renan <renanfigcarneiro@gmail.com>
2020-02-27 20:23:47 -0300

    style: markers form proposta

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
 
   const loader = document.querySelector(".loader");
-  setTimeout(() => {loader.className += " hidden";}, 1500);
+  setTimeout(() => {loader.className += " hidden";}, 500);
   
  }
 );
